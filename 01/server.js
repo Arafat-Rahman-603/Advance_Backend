5dde31ee3ec9dd3b9169cdfbf69fb6cf889beb46
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-07-08 18:35:57 +0600

    feat: initialize Express server with Docker configuration

--- 01/server.js
+++ 01/server.js
@@ -6,10 +6,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req,res)=>{
-  res.send("Hello Docker!")
-})
+  res.send("Hello Docker!");
+});
 
 app.listen(PORT,()=>{
-  console.log("Server is running on port",PORT)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
